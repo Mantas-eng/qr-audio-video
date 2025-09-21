eca82b10116e3ac87b8336918ea32d8cd4ae83a1
Mantas-eng <68243914+Mantas-eng@users.noreply.github.com>
2025-09-21 22:04:39 +0300

    updated some bugs

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
   records.forEach((rec) => {
     const col = document.createElement("div");
-    col.className = "col-md-4 mb-4";
+    col.className = "col";
 
     col.innerHTML = `
       <div class="card h-100 shadow-sm p-3 text-center">
@@ -21,28 +21,9 @@ window.addEventListener("DOMContentLoaded", () => {
     list.appendChild(col);
 
     new QRCode(document.getElementById(`qrcode-${rec.id}`), {
-      text: `https://example.com/player.html?id=${rec.id}`,
-      width: 80,
-      height: 80,
+      text: rec.file,
+      width: 100,
+      height: 100,
     });
   });
-
-  if (window.innerWidth < 768) {
-    const reader = document.getElementById("reader");
-    const html5QrCode = new Html5Qrcode("reader");
-
-    html5QrCode.start(
-      { facingMode: "environment" },
-      {
-        fps: 10,
-        qrbox: 250,
-      },
-      (decodedText, decodedResult) => {
-        window.location.href = decodedText;
-      },
-      (errorMessage) => {
-        console.log(errorMessage);
-      }
-    );
-  }
 });
